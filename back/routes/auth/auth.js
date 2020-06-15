08b8ea5e8f837b5a16577a4e0894591633ae19fd
if1311 <ioana.alexa.filip@hotmail.com>
2020-06-15 15:19:29 +0300

    post in the database

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const app = express()
 const router = express.Router();
+const connection = require("../../config")
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser)
+
 
 router.post('/signup', function (req, res, next) {
-    res.send('I am in POST signup');
+    connection.query('insert into users set?', req.body, (err, results) => {
+        if (err) {
+            console.log(err)
+            res.status(500).send("Error trying to signup")
+        } else {
+            res.status(200).send(results)
+        }
+    })
 });
 
 module.exports = router;
