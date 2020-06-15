cef7c5961f2b4e31dfb523240933116073e48711
if1311 <ioana.alexa.filip@hotmail.com>
2020-06-15 10:43:41 +0300

    form, submit, inputs

--- front/homer-odissey/src/components/SignUp.js
+++ front/homer-odissey/src/components/SignUp.js
@@ -4,18 +4,33 @@ export default class SignUp extends Component {
     constructor() {
         super()
         this.state = {
-            value: "email"
+            email: "mon@email.com",
+            password: "monPassw0rd",
+            name: "James",
+            lastname: "Bond"
         }
-        this.updateEmailField = this.updateEmailField.bind(this)
+        this.updateH1Field = this.updateH1Field.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
-    updateEmailField(event) {
-        this.setState({ value: event.target.value })
+    updateH1Field(event) {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+    handleSubmit(event) {
+        console.log('A form was submitted: ' + JSON.stringify(this.state, 1, 1));
+        event.preventDefault();
     }
     render() {
         return (
             <div>
-                <h1>{this.state.value}</h1>
-                <input type="email" name="email" onChange={this.updateEmailField} />
+                <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="lastname" name="lastname" value={this.state.lastname} onChange={this.updateH1Field} />
+                    <input type="email" name="email" value={this.state.email} onChange={this.updateH1Field} />
+                    <input type="password" name="password" value={this.state.password} onChange={this.updateH1Field} />
+                    <input type="password" />
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         )
     }
